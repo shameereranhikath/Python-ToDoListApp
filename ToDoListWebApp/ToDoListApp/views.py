7aa2e2149060bccf5fd24850dfc93ef24bbc315a
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-03-15 19:49:34 +0400

    The two more views AddItem and Delete Item is added,one for Adding item and other one for deleting the Item

--- ToDoListWebApp/ToDoListApp/views.py
+++ ToDoListWebApp/ToDoListApp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from . models import Todolist
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
     Todolistdata=Todolist.objects.all().order_by("created_date")
     return render(request,'index.html',{'Todolistdata':Todolistdata})
+
+def AddItem(request):
+    current_date=timezone.now()
+    content1=request.POST["content"]
+    Todolist.objects.create(created_date=current_date,text=content1)
+    # Todolistdata=Todolist.objects.all().order_by("created_date")
+    # return render(request,'index.html',{'Todolistdata':Todolistdata})
+    return HttpResponseRedirect('/ToDoListApp/')
+    # Redirect('/ToDoListApp/'),
+
+
+def DeleteItem(request,todo_id):
+    Todolist.objects.get(id=todo_id).delete()
+    print(todo_id)
+    return HttpResponseRedirect('/ToDoListApp/')
