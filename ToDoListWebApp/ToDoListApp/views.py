cc025916128e1754076c31eb9225e2df93d79cbd
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-03-15 19:44:55 +0400

    The index view is created to display all the data

--- ToDoListWebApp/ToDoListApp/views.py
+++ ToDoListWebApp/ToDoListApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Todolist
 
 # Create your views here.
+
+def index(request):
+    Todolistdata=Todolist.objects.all().order_by("created_date")
+    return render(request,'index.html',{'Todolistdata':Todolistdata})
